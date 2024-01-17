9329a2536b49f3bbdb8c9a77a867560069b274f5
j4s3h <johncarlsalao304@gmail.com>
2024-01-17 13:37:40 +0800

    Enchance with unread_count and read_count

--- peerkada/core/features/versions/v1p0/get_notifications/views/get_notification_views.py
+++ peerkada/core/features/versions/v1p0/get_notifications/views/get_notification_views.py
@@ -8,12 +8,26 @@ from datetime import timedelta
 from ..serializer.get_notification_serializer import NotificationSerializer
 
 class GetNotificationView(APIView):
-    
     def get(self, request):
-        user_notifications = Notification.objects.filter(user=request.user).order_by('-created_at')[:10]
-        serializer = NotificationSerializer(user_notifications, many=True)
-        data = serializer.data
+        user = request.user
+
+        # Get unread notifications
+        unread_notifications = Notification.objects.filter(user=user, is_read=False)
+
+        # Get read notifications
+        read_notifications = Notification.objects.filter(user=user, is_read=True)
+
+        # Serialize the data
+        unread_serializer = NotificationSerializer(unread_notifications, many=True)
+        read_serializer = NotificationSerializer(read_notifications, many=True)
         status = "ok"
         message = "User notifications retrieved successfully"
         errors = {}
+        data = {
+            'unread_count': unread_notifications.count(),
+            'unread_notifications': unread_serializer.data,
+            'read_count': read_notifications.count(),
+            'read_notifications': read_serializer.data,
+        }
+
         return Response({"message": message, "data": data, "status": status, "errors": errors})

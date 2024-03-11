72b6d493fc76d8837bea447b8c1998f68c00ce56
johncarls <salaojohn4@gmail.com>
2024-03-11 23:03:58 +0800

    get_stats added messsage

--- peerkada/core/features/versions/v1p0/get_stats/views/get_stats_views.py
+++ peerkada/core/features/versions/v1p0/get_stats/views/get_stats_views.py
@@ -10,6 +10,70 @@ from ..serializer.get_form_stats_serializers import DisplayFormStatsSerializer
 from django.forms.models import fields_for_model
 from django.db import models
 
+# class GetStatsView(APIView):
+#     def get_stats(self, requesting_user):
+#         try:
+#             stats = Stats.objects.filter(created_by=requesting_user.id)
+#             return stats
+#         except Stats.DoesNotExist:
+#             data = {}
+#             message = 'Oops'
+#             errors = {}
+#             status = not_Found
+#             return Response({"message": message, "data": data, "status": status, "errors": errors})
+
+#     def calculate_total_score(self, stat):
+#         total_score = 0
+#         for field in stat._meta.get_fields():
+#             if (
+#                 field.name not in ['created_by', 'id']
+#                 and not field.is_relation
+#                 and isinstance(field, models.IntegerField)
+#             ):
+#                 total_score += int(getattr(stat, field.name, 0))
+
+#         setattr(stat, 'total_score', total_score)
+
+#     def check_mental_wellbeing(self, stat):
+#         # Calculate or retrieve total score
+#         self.calculate_total_score(stat)
+
+#         # Define your mental well-being criteria
+#         if 60 <= stat.total_score <= 70:
+#             message = "You are in the optimum state of mental wellbeing! Keep it up!"
+#         elif 45 <= stat.total_score <= 55:
+#             message = "You are in a good state of mental wellbeing!"
+#         elif 14 <= stat.total_score <= 42:
+#             message = "You are showing signs of being mentally unwell. Please take a quick rest and meditate."
+#             return
+
+#         # Create a notification for the user
+#         Notification.objects.create(id = generate_uuid(), user=stat.created_by, message=message)
+
+#         # Retrieve user's notifications
+#         user_notifications = Notification.objects.filter(user=stat.created_by).order_by('created_at')
+
+#         # Delete the oldest notification if there are more than 10
+#         if user_notifications.count() > 10:
+#             oldest_notification = user_notifications.first()
+#             oldest_notification.delete()
+
+#     def get(self, request):
+#         requesting_user = request.user
+#         stats = self.get_stats(requesting_user)
+
+#         # Iterate over stats and check mental well-being for each
+#         for stat in stats:
+#             self.check_mental_wellbeing(stat)
+
+#         serializer = DisplayFormStatsSerializer(stats, many=True)
+#         data = serializer.data
+#         status = ok
+#         message = "Results"
+#         errors = {}
+
+#         return Response({"message": message, "data": data, "status": status, "errors": errors})
+
 class GetStatsView(APIView):
     def get_stats(self, requesting_user):
         try:
@@ -45,27 +109,24 @@ class GetStatsView(APIView):
             message = "You are in a good state of mental wellbeing!"
         elif 14 <= stat.total_score <= 44:
             message = "You are showing signs of being mentally unwell. Please take a quick rest and meditate."
-        elif 10 <= stat.total_score <=13:
+        elif 10 <= stat.total_score <= 13:
             message = "You are showing signs of being mentally unwell. Please take a quick rest and meditate."
-            return
-
-        Notification.objects.create(id = generate_uuid(), user=stat.created_by, message=message)
-
-        # Retrieve user's notifications
-        user_notifications = Notification.objects.filter(user=stat.created_by).order_by('created_at')
-
-        # Delete the oldest notification if there are more than 10
-        if user_notifications.count() > 10:
-            oldest_notification = user_notifications.first()
-            oldest_notification.delete()
+        else:
+            message = None  # No message if mental wellbeing is not defined
+        
+        return message
 
     def get(self, request):
         requesting_user = request.user
         stats = self.get_stats(requesting_user)
 
+        messages = []
+
         # Iterate over stats and check mental well-being for each
         for stat in stats:
-            self.check_mental_wellbeing(stat)
+            message = self.check_mental_wellbeing(stat)
+            if message:
+                messages.append(message)
 
         serializer = DisplayFormStatsSerializer(stats, many=True)
         data = serializer.data
@@ -73,4 +134,8 @@ class GetStatsView(APIView):
         message = "Results"
         errors = {}
 
+        if isinstance(data, list) and len(data) > 0 and isinstance(data[0], dict):
+            data[0]['messages'] = messages
+
         return Response({"message": message, "data": data, "status": status, "errors": errors})
+
